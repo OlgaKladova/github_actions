f1c3204861704c8f4828ebce04c4b1ffa5967365
Olga Kladova <Olenka-29-04@yandex.ru>
2024-11-03 16:02:52 +0300

    ok

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,6 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path'); //для того чтобы превратить отнсительный путь в абсолютный мы будем использовать пакет path
 const webpack = require('webpack');
+const 
 
 const production = process.env.NODE_ENV === 'production';
 
@@ -13,8 +14,8 @@ module.exports = {
 		filename: production
 			? 'static/scripts/[name].[contenthash].js'
 			: 'static/scripts/[name].js', // имя нашего бандла
-		publicPath: '/',
-		chunkFilename: 'static/scripts/[name].[contenthash].bundle.js'
+		publicPath: process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/',
+		chunkFilename: 'static/scripts/[name].[contenthash].bundle.js',
 	},
 	//Нужно помочь вебпаку научится работать с jsx и tsx файлами для этого используют ts loader
 	module: {
@@ -89,6 +90,7 @@ module.exports = {
 				: 'static/styles/[name].css',
 		}),
 		new webpack.EnvironmentPlugin({
+			PUBLIC_PATH: null,
 			NODE_ENV: 'development', // значение по умолчанию 'development' если переменная process.env.NODE_ENV не передана
 		}),
 	],
